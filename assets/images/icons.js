5e083c5cbd89f40c9ec2d4a2bd73bf1be1bb4e29
unknown <al_bacs@yahoo.com>
2025-02-10 11:14:19 +0800

    modified icons.js

--- assets/images/icons.js
+++ assets/images/icons.js
@@ -1,8 +1,9 @@
 import { AntDesign, Feather } from "@expo/vector-icons";
+import React from "react";
 
 export const icons = {
-    index: (props)=> <AntDesign name="home" size={26} {...props} />,
-    explore: (props)=> <Feather name="compass" size={26} {...props} />,
-    create: (props)=> <AntDesign name="pluscircleo" size={26} {...props} />,
-    profile: (props)=> <AntDesign name="user" size={26} {...props} />,
-}
+  index: (props) => React.createElement(AntDesign, { name: "home", size: 26, ...props }),
+  explore: (props) => React.createElement(Feather, { name: "compass", size: 26, ...props }),
+  create: (props) => React.createElement(AntDesign, { name: "pluscircleo", size: 26, ...props }),
+  profile: (props) => React.createElement(AntDesign, { name: "user", size: 26, ...props }),
+};
