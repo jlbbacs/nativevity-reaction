946d3268e3cbd4492ec9c96cbb4b41a4852d92b7
unknown <al_bacs@yahoo.com>
2025-02-10 09:27:00 +0800

    fixing error

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import ColorList from '../components/ColorList'
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
-AppRegistry.registerComponent(appName, () => App);
-AppRegistry.runApplication(appName, {
-  initialProps: {},
-  rootTag: document.getElementById('root'),
-});
+
 
 const index = () => {
   return (
