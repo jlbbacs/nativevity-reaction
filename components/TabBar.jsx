c18b565a8645854f0c4a9691cb9390d1f1deceb1
unknown <al_bacs@yahoo.com>
2025-02-10 08:46:25 +0800

    second commit

--- components/TabBar.jsx
+++ components/TabBar.jsx
@@ -1,7 +1,13 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+import { AntDesign, Feather } from '@expo/vector-icons';
+import TabBarButton from './TabBarButton';
 
 const TabBar = ({ state, descriptors, navigation }) => {
+
+
+    const primaryColor = '#0891b2';
+    const greyColor = '#737373';
   return (
     <View style={styles.tabbar}>
       {state.routes.map((route, index) => {
@@ -13,7 +19,7 @@ const TabBar = ({ state, descriptors, navigation }) => {
             ? options.title
             : route.name;
 
-        if (['_sitemap', '+not-found'].includes(route.name)) return null;
+        if(['_sitemap', '+not-found'].includes(route.name)) return null;
 
         const isFocused = state.index === index;
 
@@ -37,44 +43,64 @@ const TabBar = ({ state, descriptors, navigation }) => {
         };
 
         return (
-          <TouchableOpacity
-            key={route.key}
-            style={styles.tab}
-            accessibilityRole="button"
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarButtonTestID}
+          <TabBarButton 
+            key={route.name}
+            style={styles.tabbarItem}
             onPress={onPress}
             onLongPress={onLongPress}
-          >
-            <Text style={{ color: isFocused ? 'blue' : 'black' }}>{label}</Text>
-          </TouchableOpacity>
-        );
+            isFocused={isFocused}
+            routeName={route.name}
+            color={isFocused? primaryColor: greyColor}
+            label={label}
+          />
+        )
+
+        // return (
+        //   <TouchableOpacity
+        //     key={route.name}
+        //     style={styles.tabbarItem}
+        //     accessibilityRole="button"
+        //     accessibilityState={isFocused ? { selected: true } : {}}
+        //     accessibilityLabel={options.tabBarAccessibilityLabel}
+        //     testID={options.tabBarTestID}
+        //     onPress={onPress}
+        //     onLongPress={onLongPress}
+        //   >
+        //     {
+        //         icons[route.name]({
+        //             color: isFocused? primaryColor: greyColor
+        //         })
+        //     }
+        //     <Text style={{ 
+        //         color: isFocused ? primaryColor : greyColor,
+        //         fontSize: 11
+        //     }}>
+        //       {label}
+        //     </Text>
+        //   </TouchableOpacity>
+        // );
       })}
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
-  tabbar: {
-    position: 'absolute',
-    bottom: 25,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    marginHorizontal: 20,
-    paddingVertical: 15,
-    borderRadius: 25,
-    shadowColor: '#000',
-    shadowOpacity: 0.2,
-    shadowRadius: 25,
-  },
-  tab: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    tabbar: {
+        position: 'absolute', 
+        bottom: 25,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        marginHorizontal: 20,
+        paddingVertical: 15,
+        borderRadius: 25,
+        borderCurve: 'continuous',
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 10},
+        shadowRadius: 10,
+        shadowOpacity: 0.1
+    }
+})
 
-export default TabBar;
+export default TabBar
